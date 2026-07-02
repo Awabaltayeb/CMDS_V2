27549e9e1182927089c42d48b472329e04eab999
Awabaltayeb <110299119+Awabaltayeb@users.noreply.github.com>
2026-07-02 22:33:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # تحديث بنية الجداول وقواعد البيانات
 python manage.py migrate
+
+# إنشاء مستخدم مسؤول تلقائياً في حال لم يكن موجوداً مسبقاً
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@college.edu', 'AdminPass123!')"
