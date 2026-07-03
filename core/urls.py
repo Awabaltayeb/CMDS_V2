c6636855abe0eb3494c593eee2adad3275cd9dc8
Awabaltayeb <110299119+Awabaltayeb@users.noreply.github.com>
2026-07-03 09:20:46 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include, re_path  # أضفنا re_path هنا
+from django.urls import path, include, re_path # أضفنا re_path هنا
 from django.conf import settings
-from django.views.static import serve  # استدعاء دالة الخدمة الثابتة لتشغيل الميديا في الإنتاج
+from django.views.static import serve # استدعاء دالة الخدمة الثابتة لتشغيل الميديا في الإنتاج
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # مسارات تسجيل الدخول والخروج الافتراضية
-    path('', include('correspondence.urls')),  # مسارات تطبيق المراسلات الخاص بنا
+path('admin/', admin.site.urls),
+path('accounts/', include('django.contrib.auth.urls')), # مسارات تسجيل الدخول والخروج الافتراضية
+path('', include('correspondence.urls')), # مسارات تطبيق المراسلات الخاص بنا
 ]
 
-# كود إجبار دجانغو على قراءة وعرض ملفات الـ PDF المرفوعة على سيرفر Render حتى لو كان DEBUG=False
+كود إجبار دجانغو على قراءة وعرض ملفات الـ PDF المرفوعة على سيرفر Render حتى لو كان DEBUG=False
 urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
