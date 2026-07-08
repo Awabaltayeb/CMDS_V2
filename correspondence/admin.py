1fad720adaf48010bd1ce443035d0f011f405fa0
Awabaltayeb <110299119+Awabaltayeb@users.noreply.github.com>
2026-07-08 20:12:51 +0200

    Update admin.py

--- correspondence/admin.py
+++ correspondence/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import UserProfile, ExternalEntity, Correspondence, Directive, ReferenceCounter
+from django.contrib.auth.models import Group  # استدعاء موديل المجموعات الافتراضي
+from .models import UserProfile, ExternalEntity
 
+# إلغاء تسجيل جدول "المجموعات" الافتراضي لإخفائه من لوحة التحكم ومنع تشتيت موظف الـ IT
+try:
+    admin.site.unregister(Group)
+except admin.sites.NotRegistered:
+    pass
+
+# تسجيل الجداول المصرح لمهندس الـ IT إدارتها فقط (المستخدمون والجهات الخارجية)
 admin.site.register(UserProfile)
 admin.site.register(ExternalEntity)
-admin.site.register(Correspondence)
-admin.site.register(Directive)
-admin.site.register(ReferenceCounter)
