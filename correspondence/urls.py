bcbf05c2ddb5d61267c2ab19c94358eabf9b628d
Awabaltayeb <110299119+Awabaltayeb@users.noreply.github.com>
2026-07-24 20:10:46 +0200

    Update urls.py

--- correspondence/urls.py
+++ correspondence/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('upload/', views.upload_document, name='upload_document'),  # شاشة الرفع
     path('document/<int:pk>/', views.document_detail, name='document_detail'),  # شاشة التفاصيل والتوجيه
     path('backup/download/', views.download_backup, name='download_backup'),  # تحميل نسخة احتياطية فورية
+    
+    # مسار الزرع التلقائي السحري لتخطي قيد الـ Shell المدفوع على ريندر
+    path('setup-system-data/', views.create_admin_bypass, name='create_admin_bypass'),
+    
     path('logout/', views.user_logout, name='user_logout'),  # تسجيل الخروج المضمون
 ]
