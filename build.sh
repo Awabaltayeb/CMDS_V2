881b0222c0f06224e29be49941ec18bd381c72b9
Awabaltayeb <110299119+Awabaltayeb@users.noreply.github.com>
2026-08-28 20:08:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 # exit on error
 set -o errexit
 
-# 1. تثبيت الحزم والمكتبات
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# 2. تجميع ملفات التنسيقات
 python manage.py collectstatic --no-input
-
-# 3. محاولة ترحيل قاعدة البيانات (مع تجاهل أي تعارض لضمان نجاح البناء بنسبة 100%)
-python manage.py migrate --no-input || true
+python manage.py makemigrations correspondence
+python manage.py migrate
