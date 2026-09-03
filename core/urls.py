e10639f2e2a35b777201b4888f744963d2faf098
Awabaltayeb <110299119+Awabaltayeb@users.noreply.github.com>
2026-09-03 21:56:49 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from correspondence import views as correspondence_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  # مسارات تسجيل الدخول والخروج الافتراضية
-    path('', include('correspondence.urls')),  # مسارات تطبيق المراسلات الخاص بنا
+    path('', include('correspondence.urls')),  # مسارات تطبيق المراسلات والمؤتمرات
     
     # حماية وتأمين ملفات الـ PDF وسد ثغرة تسريب الميديا العامة برابط مباشر!
     path('media/correspondence_files/<str:filename>', correspondence_views.serve_protected_media, name='serve_protected_media'),
